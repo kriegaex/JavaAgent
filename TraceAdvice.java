788a5e1e5eeb492c978358ecfbb811d46b9cac0c
Antoine-Gicquel <toitoine_gicquel@orange.fr>
2021-08-10 11:21:15 +0200

    Create TraceAdvice.java

---
+++ TraceAdvice.java
@@ -0,0 +1,39 @@
+package src;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Stack;
+
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.*;
+
+import static net.bytebuddy.implementation.bytecode.assign.Assigner.Typing.DYNAMIC;
+
+public class TraceAdvice {
+    @Advice.OnMethodEnter
+    static void onEnter(
+        @Origin Method method,
+        @AllArguments(typing = DYNAMIC) Object[] args
+    ) {
+        Type[] ts = method.getGenericParameterTypes();
+        String[] ts_str = new String[ts.length];
+        String[] args_str = new String[args.length];
+        for (int i = 0; i<args.length; i++) {
+            ts_str[i] = ts[i].getTypeName();
+            args_str[i] = args[i].toString();
+        }
+        String currentThread = Thread.currentThread().getName();
+        if (Agent.s.containsKey(currentThread)) {
+            Agent.s.get(currentThread).add(new CompleteSTE(method.getDeclaringClass().getName(), method.getName(), ts_str, args_str));
+        } else {
+            Agent.s.put(currentThread, new Stack<CompleteSTE>());
+            Agent.s.get(currentThread).add(new CompleteSTE(method.getDeclaringClass().getName(), method.getName(), ts_str, args_str));
+        }
+    }
+
+    @Advice.OnMethodExit
+    static void onExit(@Origin Method method) {
+        String currentThread = Thread.currentThread().getName();
+        Agent.s.get(currentThread).pop();
+    }
+}
